1f2ae9abe47662fa0cd428ea97f4250c0c81192b
проход закрыт < >
2020-01-22 16:07:51 +0300

    Array7

---
+++ Array7/Array7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Console1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = N; i >= 0; i--)
+            {
+                numbers.Add(i);
+            }
+
+            foreach (int value in numbers)
+            {
+                Console.WriteLine(value);
+            }
+
+
+
+        }
+    }
+}
