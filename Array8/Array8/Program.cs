677802b33435752056cbec7c0d156b56f42e4774
проход закрыт < >
2020-01-22 16:28:05 +0300

    array8 (Недоделано)
    Недоделал

---
+++ Array8/Array8/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Console1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            int N = Convert.ToInt32(Console.ReadLine());
+            
+
+            for (int i = 1; i <= N; i++)
+            {
+                numbers.Add(i);
+            }
+            for (int i = 1; i >= N; i--)
+            {
+                numbers.Add(i);
+            }
+
+            foreach (int value in numbers)
+            {
+                Console.WriteLine(value);
+                Console.WriteLine()
+            }
+
+
+
+        }
+    }
+}
